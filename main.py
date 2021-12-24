3b84fd93d697a18d971597636efa033430e7b2a3
1pharaxh <pharaohsupreme03@gmail.com>
2021-12-24 15:55:41 -0700

    added plane/sky/model

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from ursina import *
-def update():
-    cube.x+=time.dt*.5
-    cube.y+=time.dt*.5
-    cube.z-=time.dt*.5
-app = Ursina()
+from ursina.prefabs.first_person_controller import FirstPersonController
+from ursina import * # import as usual
+app = Ursina()       # create app as usual
+grid_texture = load_texture('Assets\Textures\grid_texture.jpg')
 
-cube = Entity(model='cube', scale=(2,3,1),position=(0,0,0), rotation = (45,45,0), color=color.red)
+ground = Entity(model='plane', color=color.rgb(255,250,240), texture='grid_texture', collider='mesh', scale=(100,1,100)) # rgb vals for floral white
+player = FirstPersonController()
 
-app.run()
+box = Entity(model='cube',        #replace this for a gun.
+             origin=(0,0.7,-5),   
+             parent=camera,        
+             color=color.blue,     
+             texture='grass')     
+
+def update():   #update function for movement                  
+    if held_keys['a']:
+        camera.x -= 10 * time.dt 
+    elif held_keys['d']:
+        camera.x += 10 * time.dt
+    elif held_keys['w']:
+        camera.z += 10 * time.dt
+    elif held_keys['s']:
+        camera.z -= 10 * time.dt
+
+Sky() 
+app.run() # run
